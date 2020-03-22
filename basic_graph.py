cd5300e94d9146b62daf9ca253a7a4c7948176b2
Juanma-GP <36795142+Juanma-GP@users.noreply.github.com>
2020-03-22 16:50:00 +0100

    Modified charts
    Add new information in charts. Weighted information based on the population of each country.

--- basic_graph.py
+++ basic_graph.py
@@ -33,7 +33,7 @@ from sklearn.preprocessing import MinMaxScaler,PowerTransformer,QuantileTransfor
 
 """
 
-def get_file_of_population():
+def get_population():
     file = [file for file in os.listdir() if "Metadada" not in file and "POP.TOTL" in file]
     if len(file)==0:
         zip_file_url = "http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv"
@@ -54,21 +54,34 @@ def get_file_of_population():
         if col<max_value:
             df.drop(col,axis=1,inplace=True)
         elif col>max_value: max_value = col
+    df.columns = ['country_code_3','Latest_pop']
     return df
 
 def add_percentage_population(df):
     
-    df_population = get_file_of_population()
-    
-    df_population.columns=["country_code_3",df_population.columns[-1]]
-    
-    df_aux = df.loc[:,df.columns[-6:]]
+    df_population = get_population()
+    df_aux = df.loc[:,df.columns[-6:-2]]
     df_aux.columns = ["Latest"]+[str(name) for name in df_aux.columns[1:]]
+
     df_aux = df_aux.merge(df_population,on="country_code_3")
-    df_aux['%']= df_aux.Latest*(10**3)/df_aux[df_population.columns[-1]]
+    df_aux['%']= df_aux.Latest*(10**2)/df_aux[df_population.columns[-1]]
+
     scaler = QuantileTransformer(random_state=0)
-    df_aux["% scaled"] = scaler.fit_transform(df_aux[["%"]])
+    df_aux["Modified"] = scaler.fit_transform(df_aux[["%"]])
+    
     return df_aux
+
+def sort_columns(df,date):
+    df = df.reset_index(drop=False)\
+           .groupby(by=["index"])\
+           .sum()
+    df = df.loc[df[date] != 0, :]
+    
+    df_aux = df.T.reset_index(drop=False)
+    df_aux['index'] = pd.to_datetime(df_aux["index"])
+    df_aux = df_aux.sort_values(by="index")\
+                   .set_index("index")
+    return df_aux.T
 
 def get_country_code(data,key='confirmed'):
     return pd.DataFrame([x.get("country_code") \
@@ -77,7 +90,15 @@ def get_country_code(data,key='confirmed'):
                                  columns=['country_code'])\
              .drop_duplicates()
 
-def get_df(webpage_json,key='confirmed'):
+def resize_data(df):
+    columns_to_change = df.columns[:-5]
+    df_population = get_population()
+    df_aux=df.merge(df_population,on="country_code_3")
+    for col in columns_to_change:
+        df_aux[col] = df_aux[col]*(10**2)/df_aux["Latest_pop"]
+    return df_aux
+
+def get_df(webpage_json,key='confirmed',continent='All',mode='Absolute'):
     relevant_data = webpage_json.get(key)
     year,month,day = relevant_data.get("last_updated")\
                                   .split("T")[0]\
@@ -86,73 +107,82 @@ def get_df(webpage_json,key='confirmed'):
     date = str(int(month))+'/'+str(int(day))+'/'+year
     
     data = relevant_data.get("locations")
-
+    
+    df_countries = get_country_code(data,key)
+    
     df = pd.DataFrame([x.get("history") \
                                      for x in data],
                                  index=[x.get("country") for x in data])
     df[date]=[x.get("latest") for x in data]
     
-    df = df.reset_index(drop=False)\
-           .groupby(by=["index"])\
-           .sum()
-    df = df.loc[df[date] != 0, :]
-    df2 = df.T.reset_index(drop=False)
-    df2['index'] = pd.to_datetime(df2["index"])
-    df2 = df2.sort_values(by="index").set_index("index")
-    df = df2.T
+    df = sort_columns(df,date)
     
-    df_countries = get_country_code(data,key)
     df = df.merge(df_countries, left_index=True, right_index=True)\
            .merge(df_continents,on="country_code")
     
+    if mode != 'Absolute': df = resize_data(df)
+    
+    if continent != 'All':
+        return df.loc[df.continent==continent]\
+                 .reset_index(drop=True)
     return df
 
 
-def plot_df_covid(df,continente=None):
-    if continente in ["Americas","Africa","Asia","Europe","Oceania"]:
-        df = df.loc[df.continent==continente].reset_index(drop=True)
-    df = df.drop(["country_code","continent","sub_region"],axis=1)
+def plot_lines_covid(df,continente=None):
+    columns_to_drop = ["country_code","continent","sub_region"]
+    if "Latest_pop" in df.columns: columns_to_drop.extend(["Latest_pop"])
+    
     fig = go.Figure()
+    
+    df = df.drop(columns_to_drop,axis=1)
+    
     if continente != 'All': fig.update_layout(showlegend=True)
     else: fig.update_layout(showlegend=False)
+    
     for country in df.country:
         fig.add_trace(go.Scatter(x=df.columns, 
-                                 y=df.loc[df.country==country,:].values[0],
+                                 y=df.loc[df.country==country,df.columns[:-2]].values[0],
                                  mode='lines',
-                                 name=country))
+                                 name=df.loc[df.country==country,["country_code_3"]].values[0][0],
+                                 hovertext = [country+' - '+str(date).split()[0] \
+                                              for date in df.columns[:-2]]))
     
     fig.show()
 
-def plot_maps(df):
+def plot_maps(df,mode):
+    
+    if mode == "Absolute":
+        color_column="%"
+        title = "<b>Map with contagion rate, real percentage</b>"
+        hover_columns = ["Latest"]
+    else:
+        color_column="Modified"
+        title = "<b>Map with data modified for appreciate the order of the countries by contagion rate</b>"
+        hover_columns = ["Latest","%"]
+    
     fig = px.choropleth(df, locations="country_code_3",
-                        color="% scaled", # lifeExp is a column of gapminder
+                        color=color_column, # lifeExp is a column of gapminder
                         hover_name="country", # column to add to hover information
-                        hover_data=["Latest","%"],
+                        hover_data=hover_columns,
                         color_continuous_scale=px.colors.sequential.Plasma,
                         scope="world",
-                        title="<b>Map with data scaled to appreciate the order of the countries by contagion rate</b>")
-    fig.update_layout(title_x=0.5)
-    fig.show()
-    
-    fig = px.choropleth(df, locations="country_code_3",
-                        color="%", # lifeExp is a column of gapminder
-                        hover_name="country", # column to add to hover information
-                        hover_data=["continent","sub_region","Latest"],
-                        color_continuous_scale=px.colors.sequential.Plasma,
-                        title="<b>Map with contagion rate, real percentage</b>")
+                        title=title)
     fig.update_layout(title_x=0.5)
     fig.show()
 
-def get_graph(key,continent):
-    df = get_df(webpage_json,key)
-    plot_df_covid(df,continent)
+
+def get_graph(mode,key,continent):
+    df = get_df(webpage_json,key,continent,mode)
+    plot_lines_covid(df,continent)
     df_aux = add_percentage_population(df)
-    plot_maps(df_aux)
+    plot_maps(df_aux,mode)
+        
 
 
 
 
 # csv obtenido de Kaggle https://www.kaggle.com/statchaitya/country-to-continent/data#
+
 df_continents = pd.read_csv("countryContinent.csv",encoding = "ISO-8859-1")\
                   .loc[:,["country","code_2","code_3","continent","sub_region"]]
 df_continents.columns=["country","country_code","country_code_3","continent","sub_region"]
@@ -164,5 +194,6 @@ webpage_str = requests.get("https://coronavirus-tracker-api.herokuapp.com/all")\
 webpage_json = json.loads(webpage_str)
 
 interact(get_graph,
-         key=widgets.Dropdown(options=["confirmed", "deaths", "recovered"], value="confirmed"),
+         mode= widgets.Dropdown(options=["Absolute","Relative"], value="Absolute"),
+         key = widgets.Dropdown(options=["confirmed", "deaths", "recovered"], value="confirmed"),
          continent=widgets.Dropdown(options=["Africa","Americas","Asia","Europe","Oceania","All"], value="Europe"))
